4b283bcfe134af30ddddc68c6aa4ccc17f7321f1
BGprayer <1848744039@qq.com>
2023-02-15 16:37:25 +0800

    first commit

---
+++ Todos/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Todos
+//
+//  Created by 刘军 on 2021/11/25.
+//
+
+import UIKit
+
+let kTodoTableVCID = "TodoTableVCID"
+
+let kTodoCellID = "TodoCellID"
+let kAddTodoID = "AddTodoID"
+let kEditTodoID = "EditTodoID"
+let kTodoKey = "TodoKey"
+
+func pointIcon(_ iconName: String, _ pointSize: CGFloat = 22) -> UIImage?{
+    let config = UIImage.SymbolConfiguration(pointSize: pointSize)
+    return UIImage(systemName: iconName, withConfiguration: config)
+}
